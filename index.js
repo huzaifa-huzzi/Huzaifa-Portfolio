d1d162ea7d6e51295e15dba343b804cc1eb9cb33
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-04-16 17:05:46 +0500

    MAke Google sheets and more work on website

--- index.js
+++ index.js
@@ -34,4 +34,23 @@ function opentab(tabname){
     sidemeu.style.right = "-200px";
   }
 
+  /*--------For contact form ---------*/
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzc6z04toeVNau6m2DAQE-P46ilXzyQkzoIu3fIl26Esw1vx4yaxGsi1qOfr17gAf8B/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+          msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
 
